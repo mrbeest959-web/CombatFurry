283b9821dd73bcb772f2108381ebe4caee5ad8dc
mrbeest959-web <mrbeest959@gmail.com>
2025-11-30 18:46:51 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Это критически важно для работы на GitHub Pages и Telegram Web App
 })
